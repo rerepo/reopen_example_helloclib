d79fa72b23a50b9a8119fc67011aadc501c607b6
Marcus Tang <tmc9031@gmail.com>
2014-06-26 00:37:37 +0800

    new: host_shared_library.mk

--- Modules.mk
+++ Modules.mk
@@ -1,5 +1,18 @@
 LOCAL_PATH := $(call my-dir)
 
+###################### hlclib ######################
+include $(CLEAR_VARS)
+
+LOCAL_MODULE := hlclib
+
+LOCAL_SRC_FILES :=
+LOCAL_SRC_FILES += libso/libso.c
+
+LOCAL_C_INCLUDES :=
+LOCAL_C_INCLUDES += $(LOCAL_PATH)/libso
+
+include $(BUILD_HOST_SHARED_LIBRARY)
+
 ###################### helloclib ######################
 include $(CLEAR_VARS)
 
@@ -9,11 +22,8 @@ LOCAL_SRC_FILES :=
 LOCAL_SRC_FILES += main.c
 LOCAL_SRC_FILES += func.c
 LOCAL_SRC_FILES += lib/lib.c
-LOCAL_SRC_FILES += libso/libso.c
 
 LOCAL_C_INCLUDES :=
 LOCAL_C_INCLUDES += $(LOCAL_PATH)/lib
-LOCAL_C_INCLUDES += $(LOCAL_PATH)/libso
 
-include $(BUILD_EXECUTABLE)
-
+#include $(BUILD_HOST_EXECUTABLE)
